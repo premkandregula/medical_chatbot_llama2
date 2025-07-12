335a00b18d79f0482ff6ff91f2ae34a97a4c44f1
prem kumar <117892300+premkandregula@users.noreply.github.com>
2025-07-12 22:08:14 +0530

    commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Prem kumar',
+    author_email= 'kandregulapremkumar@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
